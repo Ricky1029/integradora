60f1d559a7471fb3b92b7adce07a109b956eb8d0
Ricky1029 <ricardobeltran035@gmail.com>
2024-06-16 14:33:27 -0700

    fondos cambiados :D

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,16 @@
 import './home.css'
-import React from 'react'
+import { React, useEffect } from 'react'
 
 const Home = () => {
+
+  useEffect(() => {
+    document.body.classList.add('body-home');
+
+    return () => {
+        document.body.classList.remove('body-home');
+    };
+  }, []);
+
   return (
     <div className='home'>Home</div>
   )
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { React, useEffect } from 'react'
 import './login.css'
 
 const Login = () => {
+
+  useEffect(() => {
+    document.body.classList.add('body-login');
+
+    return () => {
+        document.body.classList.remove('body-login');
+    };
+  }, []);
+
   return (
     <div className='login'>
       <h1>Login</h1>
