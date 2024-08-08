1aa1528678099e75ed259578ec04f523749ee906
EDWINGAA <159578458+EDWINGAA@users.noreply.github.com>
2024-08-08 13:14:39 -0700

    status de la puerta funcionando

--- src/Pages/Dashboard/DashGarage.jsx
+++ src/Pages/Dashboard/DashGarage.jsx
@@ -6,23 +6,41 @@ import BarChart from '../../Charts/Bar';
 
 const Dashboard = () => {
   const [username, setUsername] = useState('Usuario');
+  const [garageStatus, setGarageStatus] = useState(null);
 
   useEffect(() => {
-    // Aquí iría la lógica para obtener los datos del usuario
+    const fetchGarageStatus = async () => {
+      try {
+        const response = await fetch('https://api-mysql-s9hw.onrender.com/puertaS');
+        const data = await response.json();
+        console.log('Data from API:', data); // Verifica los datos recibidos
+
+        // Accediendo al primer objeto del array y obteniendo el status
+        const status = data[0]?.status;
+        setGarageStatus(status);
+      } catch (error) {
+        console.error('Error fetching garage status:', error);
+      }
+    };
+
+    fetchGarageStatus();
   }, []);
 
   return (
     <div className='dashboard'>
       <div className="list">
-        <Link to="/dashboard"><img src="src/img/dashboard.png" alt="" />Panel</Link>
-        <Link to="/garage"><img src="src/img/puerta.png" alt="" />Garaje</Link>
-        <Link to="/guests"><img src="src/img/invitado.png" alt="" />Invitados</Link>
+        <Link to="/dashboard"><img src="src/img/dashboard.png" alt="Panel" />Panel</Link>
+        <Link to="/garage"><img src="src/img/puerta.png" alt="Garaje" />Garaje</Link>
+        <Link to="/guests"><img src="src/img/invitado.png" alt="Invitados" />Invitados</Link>
       </div>
       <div className="content">
         <h1>Estado del Garaje</h1>
-        <h2>Abierto</h2>
+        <h2>{garageStatus === 1 ? 'Cerrado' : 'Abierto'}</h2>
         <div className="garage">
-            <img src="src/img/garage abierto.png" alt="" />
+          <img 
+            src={garageStatus === 1 ? 'src/img/garage cerrado.png' : 'src/img/garage abierto.png'} 
+            alt={garageStatus === 1 ? 'Garaje Cerrado' : 'Garaje Abierto'} 
+          />
         </div>
       </div>
     </div>
